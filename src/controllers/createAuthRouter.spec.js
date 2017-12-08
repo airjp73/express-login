3570477fd149cf4dd6ba55b48bf4702086ebd9a5
airjp73 <aaron.pettengill.violin@gmail.com>
2017-12-08 12:14:18 -0500

    createAuthRouter testing
    Currently, the primary router bugs out if an authRouter is app.used
    after the primary router is. Not sure why.

---
+++ src/controllers/createAuthRouter.spec.js
@@ -0,0 +1,49 @@
+var chai = require('chai')
+var chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+var expect = chai.expect
+var sinon = require('sinon')
+
+var app = require('../../exampleServer/server.js')
+var server = {}
+var User = require("../../exampleServer/models/user.js")
+var encrypt = require("../helpers/encryption.js")
+
+const TEST_USER = {
+  email: "mrTestUser@test.com",
+  profileName: "Mr. Test",
+  password: "myTestPass",
+  newPassword: "myNewTestPass",
+  confirmEmailToken: "testtest"
+}
+var mockUser = async () => {
+  var testUser = new User()
+  testUser.email = TEST_USER.email
+  testUser.password = encrypt.hashPassword(TEST_USER.password)
+  return await testUser.save()
+}
+
+describe("authRouter", () => {
+  beforeEach(() => {
+    server = chai.request.agent(app)
+  })
+
+  it("should return 401 if not logged it", async () => {
+    var res = await server.get("/auth/test/testRoute")
+
+    expect(res).to.have.status(401)
+  })
+
+  it("should return 200 if logged in", async () => {
+    await mockUser()
+    var fields = {
+      email: TEST_USER.email,
+      password: TEST_USER.password
+    }
+
+    await server.post("/auth/login").send(fields)
+    var res = await server.get("/auth/test/testRoute")
+
+    expect(res).to.have.status(200)
+  })
+})
