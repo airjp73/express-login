5b1a8d4bf47945983f783d28c788d34d23eae8ae
AaronP <aaron.pettengill.violin@gmail.com>
2017-12-15 00:11:28 -0500

    Added use function

---
+++ src/use.js
@@ -0,0 +1,10 @@
+"use strict"
+
+//Allow user to expressLogin.use() for strategies
+
+var config = require('config')
+var passport = require('passport')
+
+module.exports = (strategy) => {
+  strategy.init(config, passport)
+}
