a22e662b4fde6d4e80321660e6e5d282948f71ff
AaronP <aaron.pettengill.violin@gmail.com>
2017-12-22 15:46:14 -0500

    Added test for index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@
 
 var config = require('./config')
 var router = require('./router.js')
-var configurePassport = require("./configurePassport")
 var createAuthRouter = require('./createAuthRouter.js')
 var use = require('./use')
 
 module.exports = (options) => {
   config.init(options)
-  configurePassport(options)
 
   return {
     config,
