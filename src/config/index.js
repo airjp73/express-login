a00c3743a08b9e5b838ce443710c8cd0d14666d5
airjp73 <aaron.pettengill.violin@gmail.com>
2017-12-27 14:57:22 -0500

    No longer switching modules through options object
    Dropping in a custom database, mailer, or encryption module will no
    longer be done through options object. Instead it will be done with e.g.
    expressLogin.useDatabase(database)

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,15 @@
 "use strict"
 
-//Config object that contains the database and mailer modules
-//This is mutable by providing an options object
+//Config object that contains the modules that get used by strategies
+//These are all replacable with custom modules
 
 module.exports = {
   init(options) {
-    this.database = options.database  || require('./database.js')
-    if (!options.database)
-      this.database.init(options)
-
-    this.mailer   = options.mailer    || require('./mailer.js')
-    if (!options.mailer)
-      this.mailer.init(options)
+    this.database.init(options)
+    this.mailer.init(options)
   },
-  encrypt: require('./encryption')
+
+  database: require('./database'),
+  mailer:   require('./mailer'),
+  encrypt:  require('./encryption')
 }
