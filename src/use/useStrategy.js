14ec044778c20987bd9b110ead2814541680deac
airjp73 <aaron.pettengill.violin@gmail.com>
2017-12-29 18:39:24 -0500

    useStrategy now takes a route string
    This should help prevent strategies bumping into each other

--- src/use/useStrategy.js
+++ src/use/useStrategy.js
@@ -1,12 +1,19 @@
 "use strict"
 
-//Allow user to expressLogin.use() for strategies
+//Give a strategy to expressLogin
+//Called just like app.use or router.use
+//example: expressLogin.useStrategy('/local', localStrategy)
 
 var passport = require('passport')
 var config = require('../config')
 var router = require('../router')
 
-module.exports = (strategy) => {
-  var routes = strategy.init(config, passport)
-  router.use(routes)
+module.exports = (routeStr, strategy) => {
+  //Check if only one argument
+  //by rights, the optional argument should be second but this function mimics app.use
+  if (!strategy)
+    strategy = routeStr
+
+  var strategyRouter = strategy.init(config, passport)
+  router.use(routeStr, strategyRouter)
 }
