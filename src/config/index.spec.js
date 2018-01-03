87294559ecf623d1d672177568d7ec5a14a16f56
airjp73 <aaron.pettengill.violin@gmail.com>
2018-01-03 12:49:21 -0500

    Additional tests

--- src/config/index.spec.js
+++ src/config/index.spec.js
@@ -10,4 +10,16 @@ describe("config", () => {
   it("should return an object", () => {
     expect(index).to.be.an('object')
   })
+
+  it("init should call init on sub-modules", () => {
+    var dbinit = sinon.stub(index.database, "init")
+    var mailinit = sinon.stub(index.mailer, "init")
+
+    index.init({})
+    sinon.assert.called(dbinit)
+    sinon.assert.called(mailinit)
+
+    dbinit.restore()
+    mailinit.restore()
+  })
 })
